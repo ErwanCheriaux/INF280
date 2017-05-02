8c325fe13cbb7346f0747e47969c94418807724a
Erwan CHERIAUX <erwan.cheriaux@gmail.com>
2017-05-02 23:54:20 +0200

    4493 - wip

--- 4493-that_is_your_queue/4493-that_is_your_queue.cpp
+++ 4493-that_is_your_queue/4493-that_is_your_queue.cpp
@@ -9,23 +9,29 @@ using namespace std;
 
 int main(void)
 {
-   int P, C, numero;
+   int P, C, numero, index=1, express;
    char citizen;
 
    while(1)
    {
       scanf("%d %d\n", &P, &C);
       if(!P and !C) return 0;
-      
-      printf("%d %d\n", P, C);
+
+      printf("Case %d:\n", index);
+      express=0;
 
       for(int i=0; i<C; i++)
       {
          scanf("%c\n", &citizen);
-         if(citizen == 'E') scanf("%d\n", &numero);
-
-         if(citizen == 'E') printf("%c %d\n", citizen, numero);
-         else               printf("%c\n", citizen);
+         if(citizen == 'E') 
+         {
+            scanf("%d\n", &numero);
+            printf("%d\n", numero);
+            express++;
+         }
+         else printf("%d\n", ((i-express)%P)+1);
       }
+
+      index++;
    }
 }
