277bd4be5452349984b1061451dd4b789bae4b08
Erwan CHERIAUX <erwan.cheriaux@telecom-paristech.fr>
2017-05-02 12:14:01 +0200

    algo faux

--- 4954-lawn_mower/4954-lawn_mower.cpp
+++ 4954-lawn_mower/4954-lawn_mower.cpp
@@ -20,47 +20,44 @@ int main(void)
       scanf("%lf", &w);
       if(nx == 0 and ny == 0 and w == 0.0) return 0;
 
-//    possible_x = true;
-//    possible_y = true;
-
-//    double tx[nx];
-//    if(75/w <= nx)
-//    {
-//       for(int i=0; i<nx-1; i++) scanf("%lf", &tx[i]);
-//       sort(tx, tx+nx);
-//       
-//       for(int i=0; i<nx-1; i++)
-//       {
-//          if(tx[i+1]-tx[i] > w)
-//          {
-//             possible_x = false;
-//             break;
-//          }
-//       }
-//    } else possible_x = false;
-
-//    double ty[ny];
-//    if(100/w <= ny and !possible_x)
-//    {
-//       for(int i=0; i<ny-1; i++) scanf("%lf", &ty[i]);
-//       sort(ty, ty+ny);
-//       
-//       for(int i=0; i<ny-1; i++)
-//       {
-//          if(ty[i+1]-ty[i] > w)
-//          {
-//             possible_y = false;
-//             break;
-//          }
-//       }
-//    } else possible_y = false;
-
-      //debug
+      possible_x = true;
+      possible_y = true;
 
       double tx[nx];
       double ty[ny];
+
       for(int i=0; i<nx; i++) scanf("%lf", &tx[i]);
       for(int i=0; i<ny; i++) scanf("%lf", &ty[i]);
+
+      if(75/w <= nx)
+      {
+         sort(tx, tx+nx);
+         
+         for(int i=0; i<nx-1; i++)
+         {
+            if(tx[i+1]-tx[i] > w)
+            {
+               possible_x = false;
+               break;
+            }
+         }
+      } else possible_x = false;
+
+      if(100/w <= ny and !possible_x)
+      {
+         sort(ty, ty+ny);
+         
+         for(int i=0; i<ny-1; i++)
+         {
+            if(ty[i+1]-ty[i] > w)
+            {
+               possible_y = false;
+               break;
+            }
+         }
+      } else possible_y = false;
+
+      //debug
 
       printf("%d\n", nx);
       printf("%d\n", ny);
@@ -71,7 +68,7 @@ int main(void)
       for(int i=0; i<ny; i++) printf("%lf ", ty[i]);
       printf("\n");
 
-//    if(possible_x or possible_y) printf("YES\n");
-//    else                         printf("NO\n");
+      if(possible_x or possible_y) printf("YES\n");
+      else                         printf("NO\n");
    }
 }
