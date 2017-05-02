9b4f4b1e11518d20ec31369a4ba996f42ddf7f27
Erwan CHERIAUX <erwan.cheriaux@gmail.com>
2017-05-03 00:47:00 +0200

    2433 - wip

---
+++ 2433-no_change/2433-no_change.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <cstring>
+#include <iostream>
+#include <cstdint>
+#include <algorithm>
+
+using namespace std;
+
+int main(void)
+{
+   return 0;
+}
