04713a14d2907f432642efe96bb049b91d475ab4
Erwan CHERIAUX <erwan.cheriaux@telecom-paristech.fr>
2017-05-02 11:59:44 +0200

    4954 - wip

---
+++ 4954-lawn_mower/4954-lawn_mower.cpp
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <cstring>
+#include <iostream>
+#include <cstdint>
+#include <algorithm>
+
+using namespace std;
+
+int main(void)
+{
+   int nx, ny;
+   double w;
+   bool possible_x, possible_y;
+
+   while(1)
+   {
+      scanf("%d", &nx);
+      scanf("%d", &ny);
+      scanf("%lf", &w);
+      if(nx == 0 and ny == 0 and w == 0.0) return 0;
+
+//    possible_x = true;
+//    possible_y = true;
+
+//    double tx[nx];
+//    if(75/w <= nx)
+//    {
+//       for(int i=0; i<nx-1; i++) scanf("%lf", &tx[i]);
+//       sort(tx, tx+nx);
+//       
+//       for(int i=0; i<nx-1; i++)
+//       {
+//          if(tx[i+1]-tx[i] > w)
+//          {
+//             possible_x = false;
+//             break;
+//          }
+//       }
+//    } else possible_x = false;
+
+//    double ty[ny];
+//    if(100/w <= ny and !possible_x)
+//    {
+//       for(int i=0; i<ny-1; i++) scanf("%lf", &ty[i]);
+//       sort(ty, ty+ny);
+//       
+//       for(int i=0; i<ny-1; i++)
+//       {
+//          if(ty[i+1]-ty[i] > w)
+//          {
+//             possible_y = false;
+//             break;
+//          }
+//       }
+//    } else possible_y = false;
+
+      //debug
+
+      double tx[nx];
+      double ty[ny];
+      for(int i=0; i<nx-1; i++) scanf("%lf", &tx[i]);
+      for(int i=0; i<ny-1; i++) scanf("%lf", &ty[i]);
+
+      printf("%d\n", nx);
+      printf("%d\n", ny);
+      printf("%lf\n", w);
+
+      for(int i=0; i<nx+1; i++) printf("%lf ", tx[i]);
+      printf("\n");
+      for(int i=0; i<ny+1; i++) printf("%lf ", ty[i]);
+      printf("\n");
+
+//    if(possible_x or possible_y) printf("YES\n");
+//    else                         printf("NO\n");
+   }
+}
