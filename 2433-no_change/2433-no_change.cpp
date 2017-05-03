a99663b05caee6a32b8d88f30fe3930c62db433d
Erwan CHERIAUX <erwan.cheriaux@gmail.com>
2017-05-03 16:38:59 +0200

    2433 - lecture input

--- 2433-no_change/2433-no_change.cpp
+++ 2433-no_change/2433-no_change.cpp
@@ -9,5 +9,18 @@ using namespace std;
 
 int main(void)
 {
+   int x, v[5], k;
+
+   while(scanf("%d\n\n", &x)==1)
+   {
+      //lecture input
+      scanf("%d %d", &x, &k);
+      for(int i=0; i<k; i++) scanf("%d", &v[i]);
+
+      printf("%d %d ", x, k);
+      for(int i=0; i<k; i++) printf("%d ", v[i]);
+      printf("\n");
+   }
+
    return 0;
 }
