d95e865c48910e98f0f2dcd4f30e15faa708a607
Erwan CHERIAUX <erwan.cheriaux@gmail.com>
2017-05-03 17:38:59 +0200

    2433 - wip

--- 2433-no_change/2433-no_change.cpp
+++ 2433-no_change/2433-no_change.cpp
@@ -7,6 +7,9 @@
 
 using namespace std;
 
+void no_change(int x, int *v, int k);
+int  argent(int *v, int *c, int k);
+
 int main(void)
 {
    int x, v[5], k;
@@ -17,10 +20,42 @@ int main(void)
       scanf("%d %d", &x, &k);
       for(int i=0; i<k; i++) scanf("%d", &v[i]);
 
-      printf("%d %d ", x, k);
-      for(int i=0; i<k; i++) printf("%d ", v[i]);
-      printf("\n");
+      no_change(x, v, k);
    }
-
    return 0;
+}
+
+void no_change(int x, int *v, int k)
+{
+   //init coef
+   int c[k], y, index=0;
+   for(int i=0; i<k; i++) c[i] = 0;
+
+   while(1)
+   {
+      c[index]++;
+      y = argent(v, c, k);
+      if(x == y)
+      {
+         printf("YES\n\n");
+         return;
+      }
+      else if (x < y)
+      {
+         if(index == k-1)
+         {
+            printf("NO\n\n");
+            return;
+         }
+         c[index]--;
+         index++;
+      }
+   }    
+}
+
+int argent(int *v, int *c, int k)
+{
+   int s=0;
+   for(int i=0; i<k; i++) s += v[i]*c[i];
+   return s;
 }
