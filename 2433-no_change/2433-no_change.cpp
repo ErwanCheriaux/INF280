5e04e17908b4699cbaf77fb99b7c16b648ac18c1
Erwan CHERIAUX <erwan.cheriaux@telecom-paristech.com>
2017-05-06 16:44:58 +0200

    wip

--- 2433-no_change/2433-no_change.cpp
+++ 2433-no_change/2433-no_change.cpp
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-void no_change(int x, int *v, int k);
+bool no_change(int x, int *v, int k);
 int  argent(int *v, int *c, int k);
 
 int main(void)
@@ -21,47 +21,42 @@ int main(void)
       scanf("%d %d", &x, &k);
       for(int i=0; i<k; i++) scanf("%d", &v[i]);
 
-      no_change(x, v, k);
+      if(no_change(x, v, k)) printf("YES");
+      else                   printf("NO");
+
+      if(t != n-1) printf("\n\n");
    }
    return 0;
 }
 
-void no_change(int x, int *v, int k)
+bool no_change(int x, int *v, int k)
 {
-   //init coef
-   int c[k], y, index=0;
-   for(int i=0; i<k; i++) c[i] = 0;
+   int c[5];
+   int max = x/v[0];
 
-   while(1)
+   for(int i0=0; i0<=max; i0++)
    {
-      if(index > 0 and c[index-1] <= c[index]) 
-      {   
-         if(index == k-1)
+      c[0]=i0;
+      for(int i1=0; i1<=i0; i1++)
+      {
+         c[1]=i1;
+         for(int i2=0; i2<=i1; i2++)
          {
-            printf("NO\n\n");
-            return;
+            c[2]=i2;
+            for(int i3=0; i3<=i2; i3++)
+            {
+               c[3]=i3;
+               for(int i4=0; i4<=i3; i4++)
+               {
+                  c[4]=i4;
+                  if(x == argent(v, c, k)) return true;
+               }
+            }
          }
-         index++;
       }
-      else c[index]++;
+   }
 
-      y = argent(v, c, k);
-      if(x == y)
-      {
-         printf("YES\n\n");
-         return;
-      }
-      else if (x < y)
-      {
-         if(index == k-1)
-         {
-            printf("NO\n\n");
-            return;
-         }
-         c[index]--;
-         index++;
-      }
-   }    
+   return false;  
 }
 
 int argent(int *v, int *c, int k)
