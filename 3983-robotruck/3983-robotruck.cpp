260991e2e097d84e65ddf2fc8b947f5989b53f51
Erwan CHERIAUX <erwan.cheriaux@gmail.com>
2017-05-12 01:25:42 +0200

    robotruck - wip

--- 3983-robotruck/3983-robotruck.cpp
+++ 3983-robotruck/3983-robotruck.cpp
@@ -15,10 +15,21 @@ class Package
       Package(int x, int y, int p) : _x(x), _y(y), _p(p) {}
       virtual void display() {printf("%d, %d, %d, %d\n", _x, _y, _p, _min);}
 
+      virtual int getX() {return _x;}
+      virtual int getY() {return _y;}
+
+      virtual int distFromOrigine() {return _x + _y;}
+      virtual int distFromPackage(Package *other) {return abs(this->_x - other->getX()) + abs(this->_y - other->getY());}
+
    private:
       int _x, _y, _p;
       int _min = INT_MAX;
 };
+
+int robotruck(int numPackage, vector<Package> packages)
+{
+   return 0;
+}
 
 int main(void)
 {
@@ -31,16 +42,23 @@ int main(void)
       scanf("%d", &P);
       scanf("%d", &N);
     
-      int x, y, p;
       vector<Package> packages;
   
+      //input
       for(int i=0; i<N; i++) 
       {
+         int x, y, p;
          scanf("%d %d %d", &x, &y, &p);
          packages.push_back(Package(x,y,p));
       }
 
+      //debug
       for(auto pack : packages) pack.display();
+
+      //output
+      printf("%d", robotruck(0, packages));
+      if(t == n-1) printf("\n");
+      else         printf("\n\n");
    }
 
    return 0;
