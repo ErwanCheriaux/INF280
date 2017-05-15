d4f6c24124ab7f3fb12159dd59dbc03afd43d913
Erwan CHERIAUX <erwan.cheriaux@telecom-paristech.com>
2017-05-15 13:32:14 +0200

    5540 copying books

---
+++ 5540-copying_books/5540-copying_books.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <cstring>
+#include <iostream>
+#include <cstdint>
+#include <algorithm>
+
+using namespace std;
+
+int main(void)
+{
+   int N;
+   scanf("%d", &N);
+
+   for(int t=0; t<N; t++)
+   {
+      //lecture input
+      int m, k;
+      scanf("%d %d", &m, &k);
+
+      int p[m];
+
+      for(int i=0; i<m; i++)
+      {
+         scanf("%d", &p[i]);
+      }
+
+      //algo
+
+   }
+   return 0;
+}
