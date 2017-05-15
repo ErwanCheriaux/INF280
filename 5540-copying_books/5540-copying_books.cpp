6179129dcfb2bd9cfbe3c8aa5b48611963719e6b
Erwan CHERIAUX <erwan.cheriaux@telecom-paristech.com>
2017-05-15 18:07:23 +0200

    copying book valide input

--- 5540-copying_books/5540-copying_books.cpp
+++ 5540-copying_books/5540-copying_books.cpp
@@ -18,15 +18,32 @@ int main(void)
       int m, k;
       scanf("%d %d", &m, &k);
 
-      int p[m];
+      int p[m], somPage=0;
 
       for(int i=0; i<m; i++)
       {
          scanf("%d", &p[i]);
+         somPage = somPage + p[i];
       }
 
-      //algo
+      int    som = 0;
+      int    use = 1;
+      double div = (double)somPage/k;
 
+      for(int i=0; i<m; i++)
+      {
+         som = som + p[i];
+
+         if(use < k and som > div)
+         {
+            use++;
+            som = p[i];
+            printf("/ ");
+         }
+
+         if(i==m-1) printf("%d\n", p[i]);
+         else       printf("%d ",  p[i]);
+      }
    }
    return 0;
 }
