bda3aca0a6dda2214f40d2db24f1a7df5b955091
Erwan CHERIAUX <erwan.cheriaux@gmail.com>
2017-05-19 23:40:04 +0200

    wip

--- 5540-copying_books/5540-copying_books.cpp
+++ 5540-copying_books/5540-copying_books.cpp
@@ -4,8 +4,14 @@
 #include <iostream>
 #include <cstdint>
 #include <algorithm>
+#include <climits>
 
 using namespace std;
+
+int m, k, p[500], slash[500], best[500], contrainte;
+
+void copying_books(int scriber);
+int calculeContrainte();
 
 int main(void)
 {
@@ -15,35 +21,60 @@ int main(void)
    for(int t=0; t<N; t++)
    {
       //lecture input
-      int m, k;
       scanf("%d %d", &m, &k);
-
-      int p[m], somPage=0;
 
       for(int i=0; i<m; i++)
       {
          scanf("%d", &p[i]);
-         somPage = somPage + p[i];
+         slash[i] = i;
       }
 
-      int    som = 0;
-      int    use = 1;
-      double div = (double)somPage/k;
+      contrainte = INT_MAX;
+      copying_books(k);
 
-      for(int i=0; i<m; i++)
-      {
-         som = som + p[i];
-
-         if(use < k and som > div)
-         {
-            use++;
-            som = p[i];
-            printf("/ ");
-         }
-
-         if(i==m-1) printf("%d\n", p[i]);
-         else       printf("%d ",  p[i]);
-      }
+      //output
+      printf("%d\n", contrainte);
+      for(int i=0; i<k; i++) printf("%d, ", best[i]);
+      printf("\n\n");
    }
    return 0;
+}
+
+void copying_books(int scriber)
+{
+   if(scriber == 0)
+   {
+      int contrainteCourante = calculeContrainte();
+      if(contrainte > contrainteCourante)
+      {
+         contrainte = contrainteCourante;
+         for(int i=0; i<k; i++) best[i] = slash[i];
+      }
+      return;
+   }
+
+   int fin = m;
+   if(scriber != k) fin = slash[scriber+1];
+
+   for(int i=slash[scriber]; i<fin; i++)
+   {
+      slash[scriber] = i;
+      copying_books(scriber-1);
+   }
+}
+
+int calculeContrainte()
+{
+   int som=0, top=0, scriber=1;
+   for(int i=0; i<m; i++)
+   {
+      if(i==slash[scriber])
+      {
+         top = max(top, som);
+         som = 0;
+         scriber++;
+      }
+      som += p[i];
+   }
+   return top;
 }
