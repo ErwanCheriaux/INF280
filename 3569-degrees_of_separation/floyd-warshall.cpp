ee9a8e6ab94c421e4e454ab87332a35cc3a5163d
Erwan CHERIAUX <erwan.cheriaux@telecom-paristech.fr>
2017-05-23 09:26:49 +0200

    floyd-warshall

---
+++ 3569-degrees_of_separation/floyd-warshall.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <cstring>
+#include <iostream>
+#include <cstdint>
+#include <algorithm>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+const int N = 100;
+const int MAXN = 100;
+const int MAXLEN = 5;
+
+vector<pair<int, int>> Adj[MAXN];
+
+int Dist[MAXN][MAXN];
+void FloydWarshall()
+{
+   fill_n((int *)Dist, MAXN * MAXN, MAXLEN);
+
+   for(int u=0; u < N; u++)
+   {
+      Dist[u][u] = 0;
+      for(auto tmp : Adj[u])
+         Dist[u][tmp.first] = tmp.second;
+   }
+
+   for (int k=0; k < N; k++) // check sub-path combinations
+      for(int i=0; i < N; i++)
+         for(int j=0; j < N; j++) // concatenate paths
+            Dist[i][j] = min(Dist[i][j], Dist[i][k] + Dist[k][j]);
+}
+
+int main(void)
+{
+   return 0;
+}
