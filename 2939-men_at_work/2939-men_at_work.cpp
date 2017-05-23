0bb456dd497de36181a57f64d2d7c8cfd2826be2
Erwan CHERIAUX <erwan.cheriaux@gmail.com>
2017-05-24 00:50:22 +0200

    input

--- 2939-men_at_work/2939-men_at_work.cpp
+++ 2939-men_at_work/2939-men_at_work.cpp
@@ -9,5 +9,41 @@ using namespace std;
 
 int main(void)
 {
+   int N;
+   while(scanf("%d\n", &N)==1)
+   {
+      char maze[N][N];
+      int  work[N][N];
+
+      for(int i=0; i<N; i++) {
+         for(int j=0; j<N; j++) {
+            if(j==N-1) scanf("%c\n", &maze[i][j]);
+            else       scanf("%c",   &maze[i][j]);
+         }
+      }
+
+      for(int i=0; i<N; i++) {
+         for(int j=0; j<N; j++) {
+            char c;
+            if(j==N-1) scanf("%c\n", &c);
+            else       scanf("%c",   &c);
+            work[i][j] = atoi(c);
+         }
+      }
+
+      for(int i=0; i<N; i++) {
+         for(int j=0; j<N; j++) {
+            printf("%c", maze[i][j]);
+         }
+         printf("\n");
+      }
+
+      for(int i=0; i<N; i++) {
+         for(int j=0; j<N; j++) {
+            printf("%d", work[i][j]);
+         }
+         printf("\n");
+      }
+   }
    return 0;
 }
