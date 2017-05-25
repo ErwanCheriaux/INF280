cd2701b66b33be4dcb9049dfcb77cd861da7692c
Erwan CHERIAUX <erwan.cheriaux@telecom-paristech.fr>
2017-05-25 20:12:23 +0200

    dijkstra ok

--- 4210-almost_shortest_path/4210-almost_shortest_path.cpp
+++ 4210-almost_shortest_path/4210-almost_shortest_path.cpp
@@ -4,8 +4,21 @@
 #include <iostream>
 #include <cstdint>
 #include <algorithm>
+#include <climits>
+#include <queue>
+#include <vector>
 
 using namespace std;
+
+const int MAXN = 100;
+const int MAXLEN = INT_MAX;
+vector<pair<int,int>> Adj[MAXN];
+
+unsigned int Dist[MAXN];
+typedef pair<unsigned int, int> WeightNode; // weight goes first
+priority_queue<WeightNode, vector<WeightNode>, greater<WeightNode>> Q;
+
+void Dijkstra(int root);
 
 int main(void)
 {
@@ -21,6 +34,32 @@ int main(void)
       for(int i=0; i<M; i++)
       {
          scanf("%d %d %d", &U, &V, &P);
+         Adj[U].push_back(make_pair(V,P));
+      }
+
+      Dijkstra(S);
+      printf("shortest path : %d\n", Dist[D]);
+   }
+}
+
+void Dijkstra(int root)
+{
+   fill_n(Dist, MAXN, MAXLEN);
+   Dist[root] = 0;
+   Q.push(make_pair(0, root));
+   while(!Q.empty())
+   {
+      int u = Q.top().second;              // get node with least priority
+      Q.pop();
+      for(auto tmp : Adj[u])
+      {
+         int v = tmp.first;
+         unsigned int weight = tmp.second;
+         if (Dist[v] > Dist[u] + weight)
+         {
+            Dist[v] = Dist[u] + weight;
+            Q.push(make_pair(Dist[v], v)); // simply push, no update here
+         }
       }
    }
 }
