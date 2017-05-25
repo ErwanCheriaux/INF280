c7e62b4d5bef016c50db12d38e056a32417ca1ce
Erwan CHERIAUX <erwan.cheriaux@telecom-paristech.fr>
2017-05-25 13:42:10 +0200

    4210 - lecture input

---
+++ 4210-almost_shortest_path/4210-almost_shortest_path.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <cstring>
+#include <iostream>
+#include <cstdint>
+#include <algorithm>
+
+using namespace std;
+
+int main(void)
+{
+   while(1)
+   {
+      int N, M, S, D, U, V, P;
+      scanf("%d %d", &N, &M);
+
+      if(!N and !M) return 0;
+
+      scanf("%d %d", &S, &D);
+
+      for(int i=0; i<M; i++)
+      {
+         scanf("%d %d %d", &U, &V, &P);
+      }
+   }
+}
