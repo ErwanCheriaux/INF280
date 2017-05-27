a934cdb4d7359a163be672235ac912b4c3a5ad7e
Erwan CHERIAUX <erwan.cheriaux@telecom-paristech.fr>
2017-05-27 16:07:31 +0200

    wip

--- 2939-men_at_work/2939-men_at_work.cpp
+++ 2939-men_at_work/2939-men_at_work.cpp
@@ -4,8 +4,22 @@
 #include <iostream>
 #include <cstdint>
 #include <algorithm>
+#include <queue>
+#include <vector>
+#include <climits>
 
 using namespace std;
+
+const int MAXN = 50*50;
+const int MAXLEN = INT_MAX;
+
+vector<pair<int,int>> Adj[MAXN];
+
+unsigned int Dist[MAXN];
+typedef pair<unsigned int, int> WeightNode; // weight goes first
+priority_queue<WeightNode, vector<WeightNode>, greater<WeightNode>> Q;
+
+void Dijkstra(int root);
 
 int main(void)
 {
@@ -15,6 +29,7 @@ int main(void)
       char maze[N][N];
       char work[N][N];
 
+      //input
       for(int i=0; i<N; i++) {
          for(int j=0; j<N; j++) {
             if(j==N-1) scanf("%c\n", &maze[i][j]);
@@ -29,19 +44,43 @@ int main(void)
          }
       }
 
+      //génération liste Adj
       for(int i=0; i<N; i++) {
          for(int j=0; j<N; j++) {
-            printf("%c", maze[i][j]);
+            if(maze[i][j] == '.') {
+               if(i>0   and maze[i-1][j] == '.') Adj[10*i+j].push_back(make_pair(maze[i-1][j],work[i-1][j]));
+               if(i<N-1 and maze[i+1][j] == '.') Adj[10*i+j].push_back(make_pair(maze[i+1][j],work[i+1][j]));
+               if(j>0   and maze[i][j-1] == '.') Adj[10*i+j].push_back(make_pair(maze[i][j-1],work[i][j-1]));
+               if(j<N-1 and maze[i][j+1] == '.') Adj[10*i+j].push_back(make_pair(maze[i][j+1],work[i][j+1]));
+            }
          }
-         printf("\n");
       }
 
-      for(int i=0; i<N; i++) {
-         for(int j=0; j<N; j++) {
-            printf("%c", work[i][j]);
-         }
-         printf("\n");
-      }
+      Dijkstra(0);
+
+      printf("%d\n", Dist[N*N-1]);
    }
    return 0;
+}
+
+void Dijkstra(int root)
+{
+   fill_n(Dist, MAXN, MAXLEN);
+   Dist[root] = 0;
+   Q.push(make_pair(0, root));
+   while(!Q.empty())
+   {
+      int u = Q.top().second;              // get node with least priority
+      Q.pop();
+      for(auto tmp : Adj[u])
+      {
+         int v = tmp.first;
+         unsigned int weight = tmp.second;
+         if (Dist[v] > Dist[u] + weight)
+         {
+            Dist[v] = Dist[u] + weight;
+            Q.push(make_pair(Dist[v], v)); // simply push, no update here
+         }
+      }
+   }
 }
