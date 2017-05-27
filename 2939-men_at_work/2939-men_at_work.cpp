0a49e7ece4095bd8c317899d30aad41f1e31b5d5
Erwan CHERIAUX <erwan.cheriaux@telecom-paristech.fr>
2017-05-27 23:25:42 +0200

    wip

--- 2939-men_at_work/2939-men_at_work.cpp
+++ 2939-men_at_work/2939-men_at_work.cpp
@@ -11,17 +11,13 @@
 using namespace std;
 
 const int MAXN = 50*50;
-const int MAXLEN = INT_MAX;
 
 unsigned int time_cpt;
-vector<pair<int,int>> Adj[MAXN];
+vector<int> Adj[MAXN];
+queue<int> Q;
+bool Visited[MAXN] = {};
 
-unsigned int Dist[MAXN];
-typedef pair<unsigned int, int> WeightNode; // weight goes first
-priority_queue<WeightNode, vector<WeightNode>, greater<WeightNode>> Q;
-
-void Dijkstra(int root);
-int  getWeight(int weight);
+void BFS(int root);
 
 int main(void)
 {
@@ -50,18 +46,18 @@ int main(void)
       for(int i=0; i<N; i++) {
          for(int j=0; j<N; j++) {
             if(maze[i][j] == '.') {
-               if(i>0   and maze[i-1][j] == '.') Adj[N*i+j].push_back(make_pair(N*(i-1)+j,work[i-1][j]-48));
-               if(i<N-1 and maze[i+1][j] == '.') Adj[N*i+j].push_back(make_pair(N*(i+1)+j,work[i+1][j]-48));
-               if(j>0   and maze[i][j-1] == '.') Adj[N*i+j].push_back(make_pair(N*i+j-1  ,work[i][j-1]-48));
-               if(j<N-1 and maze[i][j+1] == '.') Adj[N*i+j].push_back(make_pair(N*i+j+1  ,work[i][j+1]-48));
+               if(i>0   and maze[i-1][j] == '.') Adj[N*i+j].push_back(N*(i-1)+j);
+               if(i<N-1 and maze[i+1][j] == '.') Adj[N*i+j].push_back(N*(i+1)+j);
+               if(j>0   and maze[i][j-1] == '.') Adj[N*i+j].push_back(N*i+j-1  );
+               if(j<N-1 and maze[i][j+1] == '.') Adj[N*i+j].push_back(N*i+j+1  );
             }
          }
       }
 
       time_cpt=0;
-      Dijkstra(0);
+      BFS(0);
 
-      printf("%d\n", Dist[N*N-1]);
+      printf("%d\n", time_cpt);
 
       //initialisation Adj
       for(int i=0; i<N*N; i++) Adj[i].clear();
@@ -73,34 +69,17 @@ int main(void)
  * Alogorithme récupéré dans les slides de cours 
  * et adapté pour les besoins du problème
  */
-void Dijkstra(int root)
+void BFS(int root)
 {
-   fill_n(Dist, MAXN, MAXLEN);
-   Dist[root] = 0;
-   Q.push(make_pair(0, root));
+   Q.push(root);
    while(!Q.empty())
    {
-      int u = Q.top().second;              // get node with least priority
-      time_cpt = Dist[u]+1;
+      int u = Q.front();
       Q.pop();
-      for(auto tmp : Adj[u])
+      if(Visited[u]) continue;
+      for(auto v : Adj[u])
       {
-         int v = tmp.first;
-         unsigned int weight = getWeight(tmp.second);
-         if (Dist[v] > Dist[u] + weight)
-         {
-            Dist[v] = Dist[u] + weight;
-            Q.push(make_pair(Dist[v], v)); // simply push, no update here
-         }
+         Q.push(v);
       }
    }
-}
-
-int getWeight(int weight)
-{
-   if(!weight) return 1;
-
-   int res = time_cpt % (weight*2);
-   if(res<weight) return 1;
-   else           return (weight*2-res+1);
 }
