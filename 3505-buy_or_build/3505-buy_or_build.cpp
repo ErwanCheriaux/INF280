9957ab9e6d0b8bc8f911b7f8d17672917b4d6232
Erwan CHERIAUX <erwan.cheriaux@gmail.com>
2017-06-01 01:00:31 +0200

    calcule des poids

--- 3505-buy_or_build/3505-buy_or_build.cpp
+++ 3505-buy_or_build/3505-buy_or_build.cpp
@@ -61,8 +61,8 @@ int main(void)
       }
 
       //calcule du poid des arrètes
-      for(int i=1; i<n+1; i++)
-         for(int j=1; j<n+1; j++)
+      for(int i=0; i<n; i++)
+         for(int j=0; j<n; j++)
             if(i!=j)
                Edges.push_back(make_pair(weight(i,j), make_pair(i,j)));
 
