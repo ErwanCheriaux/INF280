b75cb003d537b237e94a193642c9f72bdfcdb57f
Erwan CHERIAUX <erwan.cheriaux@telecom-paristech.com>
2017-06-03 17:07:36 +0200

    modif Union, supp Find

--- 3505-buy_or_build/3505-buy_or_build.cpp
+++ 3505-buy_or_build/3505-buy_or_build.cpp
@@ -7,7 +7,6 @@
 #include <climits>
 #include <cmath>
 #include <vector>
-#include <map>
 
 using namespace std;
 
@@ -21,14 +20,12 @@ struct subnetwork
    vector<int> cities;
 } subnetworks[MAXQ];
 
-map<int, pair<int,unsigned int>> Sets_ref;
-map<int, pair<int,unsigned int>> Sets;  // map to parent & rank
+vector<int> Sets;
 vector<pair<int, pair<int,int>>> Edges;
 
 vector<pair<int,int>> Borduria;
 int T, n, q, best_global, best_local;
 
-int  Find(int x);
 void Union(int x, int y);
 int  weight(int x, int y);
 void Kruskal();
@@ -58,15 +55,12 @@ int main(void)
          int x, y;
          scanf("%d %d\n", &x, &y);
          Borduria.push_back(make_pair(x, y));
-         Sets_ref.insert(make_pair(i, make_pair(i, 0)));
       }
 
       //calcule du poid des arrètes
       for(int i=0; i<n-1; i++)
-      {
          for(int j=i+1; j<n; j++)
             Edges.push_back(make_pair(weight(i,j), make_pair(i,j)));
-      }
 
       best_global = INT_MAX;
       int maxT = pow(2, q);
@@ -76,7 +70,8 @@ int main(void)
       for(int i=0; i<maxT; i++)
       {
          //initialisation
-         Sets = Sets_ref;
+         Sets.clear();
+         for(int j=0; j<n; j++) Sets.push_back(j);
          best_local = 0;
          //parcours des compagnies de télécommunication
          for(int j=0; j<q; j++)
@@ -118,23 +113,16 @@ int weight(int x, int y)
           (Borduria[x].second - Borduria[y].second);
 }
 
-int Find(int x) 
-{
-   auto &tmp = Sets[x];
-   if(tmp.first == x) return x;                  // Parent == x ?
-   else return tmp.first = Find(tmp.first);  // Get Parent
-}
-
 void Union(int x, int y)
 {
-   int parentX = Find(x), parentY = Find(y);
-   if(parentX == parentY) return;
+   int ensembleX = Sets[x];
+   int ensembleY = Sets[y];
 
-   auto &tmpX = Sets[parentX], &tmpY = Sets[parentY];
-   int rankX = tmpX.second, rankY = tmpY.second;
-   if(rankX < rankY)  tmpX.first = parentY;
-   else               tmpY.first = parentX;
-   if(rankX == rankY) tmpX.second++;
+   if(ensembleX == ensembleY) return;
+
+   for(auto &tmp : Sets)
+      if(tmp == ensembleY)
+         tmp = ensembleX;
 }
 
 /* 
@@ -146,7 +134,7 @@ void Kruskal()
    for(auto tmp : Edges)
    {
       auto edge = tmp.second;
-      if(Find(edge.first) != Find(edge.second)) 
+      if(Sets[edge.first] != Sets[edge.second]) 
       {
          Union(edge.first, edge.second); // update Union-Find DS
          best_local += tmp.first; //somme du poid de chaque arrète de l'arbre
