6f3d757dc18c6eae18719f23cd97355ec764dfe4
Erwan CHERIAUX <erwan.cheriaux@telecom-paristech.com>
2017-06-03 19:36:15 +0200

    lecture input

--- 3126-taxi_cab_scheme/3126-taxi_cab_scheme.cpp
+++ 3126-taxi_cab_scheme/3126-taxi_cab_scheme.cpp
@@ -11,7 +11,42 @@
 
 using namespace std;
 
+const int MAXM = 500-1;
+
+struct
+{
+   int start_time;
+   int start_x;
+   int start_y;
+   int destination_time;
+   int destination_x;
+   int destination_y;
+} Taxis[MAXM];
+
+int N, M;
+
 int main(void)
 {
+   scanf("%d\n", &N);
+   for(int n=0; n<N; n++)
+   {
+      scanf("%d\n", &M);
+      for(int m=0; m<M; m++)
+      {
+         int heure, minute, st, sx, sy, dt, dx, dy;
+         scanf("%d:%d %d %d %d %d\n", &heure, &minute, &sx, &sy, &dx, &dy);
+
+         st = 60*heure+minute;
+         dt = st + abs(sx - dx) + abs(sy - dy);
+
+         Taxis[m].start_time = st;
+         Taxis[m].start_x = sx;
+         Taxis[m].start_y = sy;
+         Taxis[m].destination_time = dt;
+         Taxis[m].destination_x = dx;
+         Taxis[m].destination_y = dy;
+      }
+   }
+
    return 0;
 }
