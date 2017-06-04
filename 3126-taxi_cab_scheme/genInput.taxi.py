803e64d07ae0177d5d28ea59202b34906f1da32c
Erwan CHERIAUX <erwan.cheriaux@telecom-paristech.com>
2017-06-04 03:12:20 +0200

    gen input

---
+++ 3126-taxi_cab_scheme/genInput.taxi.py
@@ -0,0 +1,38 @@
+from random import randint
+
+f = open("taxi.input","w")
+
+N = 1
+heure = []
+minute = []
+
+for i in range(N):
+   M = 499
+
+   f.write(str(N) + "\n")
+   f.write(str(M) + "\n")
+
+   for j in range(M):
+      heure.append(randint(0, 23))
+      minute.append(randint(0, 59))
+
+   heure.sort()
+   minute.sort()
+
+   for j in range(M):
+      if heure[j] < 10:
+         f.write("0" + str(heure[j]) + ":")
+      else:
+         f.write(str(heure[j]) + ":")
+
+      if minute[j] < 10:
+         f.write("0" + str(minute[j]))
+      else:
+         f.write(str(minute[j]))
+
+      for k in range(4):
+         f.write(" " + str(randint(0, 199)))
+
+      f.write("\n")
+
+f.close()
