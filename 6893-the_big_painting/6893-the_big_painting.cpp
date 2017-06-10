ded5a0cccb2b51159f7f2e935d9bf4cd88d7c5b5
Erwan CHERIAUX <erwan.cheriaux@gmail.com>
2017-06-10 17:24:30 +0200

    wip

--- 6893-the_big_painting/6893-the_big_painting.cpp
+++ 6893-the_big_painting/6893-the_big_painting.cpp
@@ -13,28 +13,59 @@ using namespace std;
 
 const int MAXN = 2000;
 
-char p[MAXN], s[MAXN]; int T[MAXN+1];
-char motif[MAXN][MAXN], tab[MAXN][MAXN];
-int np = strlen(p), ns = strlen(s);
+int T[MAXN+1];
+char motif[MAXN][MAXN];
 
 int hp, wp, hm, wm;
+
+void makeTable()
+{
+   T[0] = -1;
+   int cnd = 0;
+   for (int i = 1; i < wp; i++)
+   {
+      T[i] = cnd;
+      while (cnd >= 0 && motif[cnd][0] != motif[i][0])
+         cnd = T[cnd];
+      cnd++;
+   }
+}
+
+void KnuthMorrisPratt()
+{
+   int cnd = 0;                          // position courante dans le motif
+   for (int i = 0; i < (hm*(wm+1)); i++)
+   {
+      char tableau;
+      scanf("%c", &tableau);
+      while (cnd >= 0 && motif[cnd][0] != tableau) // tant qu'on ne lit pas le prochain char du motif
+         cnd = T[cnd];                             // on recule dans le motif
+      cnd++;         // maintenant que le prochain char convient, avancer
+      if (cnd == hp) // on a atteint la fin de p, donc on a trouvé un match
+      {
+         // on recule dans p au cas où le prochain match chevauche
+         printf("match at %d\n", i - hp + 1);
+         cnd = T[cnd];
+      }
+   }
+}
 
 int main(void)
 {
    while(scanf("%d %d %d %d\n", &hp, &wp, &hm, &wm) == 1)
    {
+      //lecture motif
       for(int i=0; i<hp; i++){
          for(int j=0; j<wp; j++)
             scanf("%c", &motif[i][j]);
          scanf("\n");
       }
 
-      for(int i=0; i<hm; i++){
-         for(int j=0; j<wm; j++)
-            scanf("%c", &tab[i][j]);
-         scanf("\n");
-      }
-            
+      //calcule à partir du motif
+      makeTable();
+
+      //lecture tableau
+      KnuthMorrisPratt();
    }
    return 0;
 }
