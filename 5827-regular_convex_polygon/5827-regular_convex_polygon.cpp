e87848f0b58bc3a8f5bbe06fbcd4f68cd9460465
Erwan CHERIAUX <erwan.cheriaux@gmail.com>
2017-06-13 16:54:11 +0200

    barycentre wip

--- 5827-regular_convex_polygon/5827-regular_convex_polygon.cpp
+++ 5827-regular_convex_polygon/5827-regular_convex_polygon.cpp
@@ -14,7 +14,7 @@ using namespace std;
 
 #define PI 3.14159265
 
-double x[3], y[3];
+double x[4], y[4];
 
 inline double produitScalaire(int u, int v)
 {
@@ -28,7 +28,31 @@ inline double norme(int u)
 
 double angle(int u, int v)
 {
-   return acos(produitScalaire(u,v)/(norme(u)*norme(v))) * 180.f / (2*PI);
+   return acos(produitScalaire(u,v)/(norme(u)*norme(v)));
+}
+
+void barycentre()
+{
+   double angles[3];
+   double lambda = 0;
+   x[3] = 0;
+   y[3] = 0;
+   for(int i=0; i<3; i++) angles[i] = angle((i+1)%3,(i+2)%3);
+   for(int i=0; i<3; i++) lambda += 2*tan(angles[i]);
+   for(int i=0; i<3; i++)
+   {
+      x[3] += (tan(angles[(i+1)%3]) + tan(angles[(i+2)%3])) * x[i] / lambda;
+      y[3] += (tan(angles[(i+1)%3]) + tan(angles[(i+2)%3])) * y[i] / lambda;
+   }
+
+   cout
+      << angles[0] << ", "
+      << angles[1] << ", "
+      << angles[2] << ", "
+      << tan(angles[0]) << ", "
+      << tan(angles[1]) << ", "
+      << tan(angles[2]) << ", "
+      << lambda << endl;
 }
 
 int main(void)
@@ -44,10 +68,8 @@ int main(void)
       for(int i=1; i<3; i++) cin >> x[i] >> y[i];
 
       //recherche du centre du cercle circonscrit
-      cout 
-         << angle(0,1) << ", "
-         << angle(1,2) << ", "
-         << angle(2,0) << ", "
-         << endl;
+      barycentre();
+
+      cout << "{" << x[3] << "," << y[3] << "}" << endl;
    }
 }
