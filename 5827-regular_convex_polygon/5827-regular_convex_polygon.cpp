2c1d3e894c4c5d08f61c6b3211bb51dcedab9504
Erwan CHERIAUX <erwan.cheriaux@telecom-paristech.fr>
2017-06-13 10:44:42 +0200

    lecture input

--- 5827-regular_convex_polygon/5827-regular_convex_polygon.cpp
+++ 5827-regular_convex_polygon/5827-regular_convex_polygon.cpp
@@ -11,7 +11,22 @@
 
 using namespace std;
 
+double x[3], y[3];
+
 int main(void)
 {
-   return 0;
+   while(1)
+   {
+      string str;
+      cin >> str >> y[0];
+      if(str == "END") return 0;
+
+      x[0] = atof(str.c_str());
+      for(int i=1; i<3; i++) cin >> x[i] >> y[i];
+
+      printf("{%f,%f,%f}\n", x[0], x[1], x[2]);
+      printf("{%f,%f,%f}\n", y[0], y[1], y[2]);
+
+      scanf("\n");
+   }
 }
