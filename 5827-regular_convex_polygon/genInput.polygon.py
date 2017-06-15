3696ed2500717f753427ed6b111863d962674380
Erwan CHERIAUX <erwan.cheriaux@gmail.com>
2017-06-16 01:12:08 +0200

    gen update

--- 5827-regular_convex_polygon/genInput.polygon.py
+++ 5827-regular_convex_polygon/genInput.polygon.py
@@ -1,13 +1,16 @@
 from random import *
+from math import *
+
 
 f = open("polygon.input","w")
 
-for n in range(1):
+for n in range(50):
    for i in range(3):
       for j in range(2):
          a = random()
          b = randint(-10000,10000)
-         f.write(str(a*b))
+         c = round(a*b,6)
+         f.write(str(c))
          if j == 0:
             f.write(" ")
       f.write("\n")
