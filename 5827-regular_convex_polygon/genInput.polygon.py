ac6c1238bb6f7e16187995ff8fe59092f32ec992
Erwan CHERIAUX <erwan.cheriaux@gmail.com>
2017-06-16 01:26:11 +0200

    error

--- 5827-regular_convex_polygon/genInput.polygon.py
+++ 5827-regular_convex_polygon/genInput.polygon.py
@@ -4,16 +4,20 @@ from math import *
 
 f = open("polygon.input","w")
 
-for n in range(50):
+for n in range(5):
    for i in range(3):
       for j in range(2):
          a = random()
-         b = randint(-10000,10000)
-         c = round(a*b,6)
+         b = randint(-10,10)
+         c = round(a*b)
          f.write(str(c))
          if j == 0:
             f.write(" ")
       f.write("\n")
+
+f.write("10 0\n")
+f.write("0 10\n")
+f.write("0 -10\n")
 
 f.write("END\n")
 
