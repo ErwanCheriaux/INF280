6a4ffdeb561f3ee1da782bca36f9ce335dfad4f8
Erwan CHERIAUX <erwan.cheriaux@gmail.com>
2017-06-20 14:43:39 +0200

    A000788

--- 6527-counting_ones/6527-counting_ones.cpp
+++ 6527-counting_ones/6527-counting_ones.cpp
@@ -11,7 +11,25 @@
 
 using namespace std;
 
+unsigned long long A, B;
+
+/* source : http://oeis.org/A000788/a000788.txt */
+unsigned long long A000788(unsigned long long n)
+{
+   unsigned long long v = 0;
+   for (unsigned long long bit = 1; bit <= n; bit <<= 1)
+      v += ((n>>1)&~(bit-1)) + ((n&bit) ? (n&((bit<<1)-1))-(bit-1) : 0);
+   return v;
+}
+
 int main(void)
 {
-   return 0;
+   while(1)
+   {
+      if(!(cin >> A >> B)) return 0;
+
+      cout << A << " : " << A000788(A) << endl;
+      cout << B << " : " << A000788(B) << endl;
+      cout << A000788(B) - A000788(A) + 1 << endl;
+   }
 }
