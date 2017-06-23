5c7fd4e8b185695a489aab525e32e3129a16b1f9
Erwan CHERIAUX <erwan.cheriaux@telecom-paristech.com>
2017-06-23 21:34:32 +0200

    recherche dichotomique

--- 2565-calling_extraterrestrial_intelligence_again/2565-calling_extraterrestrial_intelligence_again.cpp
+++ 2565-calling_extraterrestrial_intelligence_again/2565-calling_extraterrestrial_intelligence_again.cpp
@@ -11,10 +11,10 @@
 
 using namespace std;
 
-bitset<10000001> P;
 vector<long long> premiers;
 
 void cribleEratosthene(int pmax);
+int  rechercheDicho(int val, int nbval);
 
 unsigned int M, A, B;
 
@@ -27,14 +27,19 @@ int main(void)
       cin >> M >> A >> B;
       if(!M and !A and !B) return 0;
 
-      cout << M << endl;
-      cout << A << endl;
-      cout << B << endl;
+      double C = A/B;
+      int indexMax = rechercheDicho(M/2, premiers.size());
+
+      cout << "le nombre premier le plus proche de " << M/2 << " est " << premiers[indexMax] << "(" << indexMax << ")" << endl;
    }
 }
 
+/* 
+ * Alogorithme récupéré dans les slides de cours 
+ */
 void cribleEratosthene(int pmax)
 {
+   bitset<10000001> P;
    P.set(); // initialisation
    P[0] = P[1] = 0;
 
@@ -47,3 +52,26 @@ void cribleEratosthene(int pmax)
       }
    }
 }
+
+/* 
+ * Alogorithme récupéré sur le site d'openclassroom
+ */
+int rechercheDicho(int val, int nbval)
+{
+   bool trouve = false; //vaut faux tant que la valeur "val" n'aura pas été trouvée
+   int  id     = 0;     //indice de début
+   int  ifin   = nbval; //indice de fin
+   int  im;              //indice de "milieu"
+
+   while(!trouve && ((ifin - id) > 1))
+   {
+      im = (id + ifin)/2;          //on détermine l'indice de milieu
+      trouve = (premiers[im] == val);   //on regarde si la valeur recherchée est à cet indice
+      if(premiers[im] > val) ifin = im; 
+      else id = im;
+   }
+
+   return id;  //on retourne l'indice de la valeur la plus proche inférieur ou éguale
+}
+
+
