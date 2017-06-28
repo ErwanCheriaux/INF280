79234b6cc9dbd08d4dd02b76928a7e48ae396a2f
Erwan CHERIAUX <erwan.cheriaux@telecom-paristech.fr>
2017-06-28 21:03:14 +0200

    new documents

--- template.cpp
+++ template.cpp
@@ -1,17 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <cstring>
 #include <iostream>
 #include <cstdint>
+#include <cstring>
 #include <algorithm>
+#include <utility>
 #include <climits>
-#include <queue>
 #include <vector>
+#include <queue>
 #include <list>
 
 using namespace std;
 
 int main(void)
 {
+   cin >> var;        //input
+   scanf("%d", &var); //input
+
+   cout << var << endl;  //output
+   fprintf("%d\n", var); //output
+
    return 0;
 }
